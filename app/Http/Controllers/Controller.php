5ad9731c7dff0338c6082889f1fd74116c0f1e80
Nyemotion <tntuyen.khmt0115@student.ctuet.edu.vn>
2018-06-15 09:31:28 +0700

    rebuilt whole project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function trangchu(){
-    	return view('index');
-    }
-
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
